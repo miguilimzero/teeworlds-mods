d6014545507c29cf748cd9c77ea66b165ef49381
srdante <minidantebtc@gmail.com>
2022-03-04 02:19:22 -0300

    update bam permission for infclass

--- infclass.sh
+++ infclass.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Install Dependencies
 wget https://raw.githubusercontent.com/teeframe/mods-compiler/main/default-dependencies.sh  -O - | sh
-
 sudo apt-get install -y libicu-dev libmaxminddb-dev
 
+# Clone Repository
 git clone https://github.com/InfectionDust/teeworlds-infclassr -b staging
 cd teeworlds-infclassr
 
+# Download Bam Binary
 wget https://raw.githubusercontent.com/teeframe/mods-compiler/main/bam
+chmod 764 bam
 
+# Compile mod
 ./bam server_debug
