9ec37abbd891b96f995d8d6105670debb101207e
srdante <minidantebtc@gmail.com>
2022-03-05 02:52:09 -0300

    update infclass renaming

--- 0.6.x/infclass/compile.sh
+++ 0.6.x/infclass/compile.sh
@@ -10,6 +10,6 @@ cmake ..
 make -j$(nproc)
 
 # Move Binary & Delete Repository
-mv Infclass-Server ../../Infclass-Server
+mv Infclass-Server ../../InfClass-Server
 cd ../..
 rm -rf teeworlds-infclassr
