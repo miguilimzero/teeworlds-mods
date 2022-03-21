61522b5807d9412fcd2d56ab13185a15242789e7
srdante <minidantebtc@gmail.com>
2022-03-20 22:41:11 -0300

    add compiling staging for infclass & ddnet

---
+++ 0.6.x/ddnet/compile-staging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clone Repository
+git clone https://github.com/ddnet/ddnet --recurse-submodules -b staging
+cd ddnet
+
+# Compile Mod
+mkdir build && cd build
+cmake -DCLIENT=OFF -DMYSQL=ON ..
+make -j$(nproc)
+
+# Move Binary & Delete Repository
+mv DDNet-Server ../../DDNet-Server
+cd ../..
+rm -rf ddnet
---
+++ 0.6.x/infclass/compile-staging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clone Repository
+git clone https://github.com/InfectionDust/teeworlds-infclassr -b staging
+cd teeworlds-infclassr
+
+# Compile Mod
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+
+# Move Binary & Delete Repository
+mv Infclass-Server ../../InfClass-Server
+cd ../..
+rm -rf teeworlds-infclassr
