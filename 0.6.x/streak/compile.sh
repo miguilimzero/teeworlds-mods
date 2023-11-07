1977fea3577d2eb3cf414111c6fcb39b995ead4a
SypFilter <gastomafo@gmail.com>
2023-11-07 11:26:12 -0500

    Add streak mod

---
+++ 0.6.x/streak/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Clone Repository
+git clone https://github.com/miukmiuk/teeworlds_streak.git
+cd teeworlds_streak
+
+# Download Bam Binary
+wget https://raw.githubusercontent.com/teeframe/mods-repository/main/resources/bam
+chmod 764 bam
+
+# Compile Mod
+./bam server_release
+
+# Move Binary & Delete Repository
+mv teeworlds_srv ../teeworlds_srv
+cd ..
+rm -rf teeworlds_streak
