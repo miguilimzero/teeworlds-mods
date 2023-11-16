9965e14be9c3d5ce0d8ea46c15a1183db7060c03
SypFilter <gastomafo@gmail.com>
2023-11-16 17:08:03 -0500

    Fix language errors

--- installer.sh
+++ installer.sh
@@ -3,13 +3,13 @@
 REPO_DIR=$PWD
 
 list_versions() {
-    echo "Disponible versions:"
+    echo "Available versions:"
     ls -d */ | grep -vE '0\.6\.x-bin|0\.7\.x-bin|resources'
 }
 
 list_mod_per_version() {
     version=$1
-    echo "Disponible mods to the $version:"
+    echo "Mods available for $version:"
     ls -d "$REPO_DIR/$version"/*/
 }
 
@@ -20,19 +20,19 @@ install_mod() {
     if [ -d "$mod_path" ]; then     
         cd "$REPO_DIR/$version/$mod"
         bash compile.sh
-        echo "Sucess: The $mod has been instaled!"
+        echo "Success: $mod has been installed!"
         exit
     else
-        echo "Erro: Somenthing goes off."
+        echo "Error: Something went wrong."
     fi
 }
 
 
-echo "Welcome to the installer of teeworlds-mods!"
+echo "Welcome to the teeworlds-mods installer!"
 list_versions
 
 while true; do
-    echo "Put the version of the mod below (or say exit to quit):"
+    echo "Enter the mod version below (or say exit to quit):"
     read choose_version
 
     if [ "$choose_version" == "exit" ]; then
@@ -43,7 +43,7 @@ while true; do
     if [ -d "$REPO_DIR/$choose_version" ]; then
         list_mod_per_version "$choose_version"
         
-        echo "Type the mod you want to install (or say 'leave' to choose another version):"
+        echo "Enter the mod you want to install (or say 'leave' to choose another version):"
         read choose_mod
 
         if [ "$choose_mod" == "leave" ]; then
